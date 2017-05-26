5981676778c6fb1a863a656f1d7ea7d9c333b2c2
Петя Николова <nikolova.petq@gmail.com>
2017-05-26 21:14:56 +0300

    change project Gecko

--- Blog-Skeleton/Blog.UI.Tests/BrowserHost.cs
+++ Blog-Skeleton/Blog.UI.Tests/BrowserHost.cs
@@ -4,39 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestStack.Seleno;
 using TestStack.Seleno.Configuration;
-using OpenQA.Selenium.Remote;
 
 namespace Blog.UI.Tests
 {
     public static class BrowserHost
     {
         public static readonly SelenoHost Instance = new SelenoHost();
-        //public static readonly string RootUrl = @"http://localhost:60634/Article/List";        
+        public static readonly string RootUrl = @"http://localhost:60634/Article/List";
 
         static BrowserHost()
         {
-            // DesiredCapabilities capabilities = DesiredCapabilities.Chrome();
-            //  ChromeOptions options = new ChromeOptions();
-            //  options.BinaryLocation = @"C:\Users\Petya\AppData\Local\Google\Chrome\Application\chrome.exe";
-            //options.BinaryLocation = @"..\\packages\\Selenium.WebDriver.ChromeDriver.2.29.0\\driver\\win32\\chromedriver.exe";
-            //options.AddArguments(@"--incognito");
-            // capabilities.SetCapability(ChromeOptions.Capability, options);
-
-            //  System.Environment.SetEnvironmentVariable("webdriver.chrome.‌​driver", @"..\\packages\\Selenium.WebDriver.ChromeDriver.2.29.0\\driver\\win32\\chromedriver.exe");
-
-            //Use different port for instance from localhost, because there is a discrepancy using TeamCity
-
-
-            // Instance.Run("Blog", 60634, w => w.WithRemoteWebDriver(BrowserFactory.Chrome));
-
-            //Instance.Run("Blog", 60638, w => w.WithRemoteWebDriver(BrowserFactory.Chrome));
-            //  Instance.Run("Blog", 60638, w => w.WithRemoteWebDriver(() => new ChromeDriver(options)));
-            Instance.Run("Blog", 60638, w => w.WithRemoteWebDriver(() => new ChromeDriver()));
-
-            // Instance.Run("Blog", 60638);
-            // For Chrome: Instance.Run("Blog", 60634, w => w.WithRemoteWebDriver(BrowserFactory.Chrome));
-            // or Instance.Run("Blog", 60634, w => w.WithRemoteWebDriver(() => new ChromeDriver()));
+            Instance.Run("Blog", 60634, w => w.WithRemoteWebDriver(() => new ChromeDriver()));
+            //Instance.Run("Blog", 60634, w => w.WithRemoteWebDriver(BrowserFactory.Chrome));
 
             //RootUrl = Instance.Application.Browser.Url;
         }
--- Blog-Skeleton/Blog.UI.Tests/UITests.cs
+++ Blog-Skeleton/Blog.UI.Tests/UITests.cs
@@ -7,55 +7,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Blog.UI.Tests.Pages.HomePage;
-using OpenQA.Selenium.Remote;
-using OpenQA.Selenium.Support.PageObjects;
 
 namespace Blog.UI.Tests
 {
     [TestFixture]
     public class UITests
     {
-        IWebDriver driver;
-        WebDriverWait wait;
-
-        [SetUp]
-        public void Init()
-        {
-            // this.driver = new ChromeDriver();
-            this.driver = BrowserHost.Instance.Application.Browser;
-            this.wait = new WebDriverWait(this.driver, TimeSpan.FromSeconds(300));            
-        }
-
-        [TearDown]
-        public void CleanUp()
-        {
-            this.driver.Quit();
-        }
-
         [Test]
         public void CheckSiteLoad()
-        {/*
+        {
             IWebDriver driver = BrowserHost.Instance.Application.Browser;
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(300));
-            */
-            this.driver.Navigate().GoToUrl(@"http://localhost:60634/Article/List");
-            
-            var logo = this.wait.Until(w => w.FindElement(By.XPath("/html/body/div[1]/div/div[1]/a")));
+
+
+            driver.Navigate().GoToUrl("@http://localhost:60634/Article/List");
+
+            var logo = wait.Until(w => w.FindElement(By.XPath("/html/body/div[1]/div/div[1]/a")));
 
             Assert.AreEqual("SOFTUNI BLOG", logo.Text);
-        }
-        
-        [Test]
-        public void BlogLogoDisplayedRightMessage()
-        {
-            var homePage = new HomePage(this.driver);
-
-            PageFactory.InitElements(this.driver, homePage);
-
-            homePage.NavigateTo();
-
-            homePage.AssertLogo();
         }
     }
 }
